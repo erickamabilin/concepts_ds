4ae01a4433828732ffa077c7e6c3c038706ccf22
andrea-maza-leon <andrea.mazaleon@student.uhasselt.be>
2024-04-26 16:17:23 -0700

    Fix usage of check method in BloomFilter example

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,22 +1,23 @@
-class SimpleBloomFilter:
+class BloomFilter:
     def __init__(self, size):
+        self.size = size
         self.bit_array = [0] * size
 
     def add(self, item):
-        index = hash(item) % len(self.bit_array)
+        index = hash(item) % self.size
         self.bit_array[index] = 1
 
     def check(self, item):
-        index = hash(item) % len(self.bit_array)
+        index = hash(item) % self.size
         return self.bit_array[index] == 1
 
 # Example 
-bloom = SimpleBloomFilter(10)  # just for demostration
+bloom = BloomFilter(10)  # just for demostration
 
 bloom.add("apple")
 bloom.add("banana")
 
 # Check for existing and non-existing items
-print("apple" in bloom)  # Should be True
-print("banana" in bloom)  # Should be True
-print("cherry" in bloom)  # Should be False
+print("apple", bloom.check("apple"))  # Should be True
+print("banana", bloom.check("banana"))  # Should be True
+print("cherry", bloom.check("cherry"))  # Should be False
