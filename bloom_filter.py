7609e9cff600d68136657eb655a2b3534b6f0389
erickamabilin <nikka_mabilin@yahoo.com>
2024-05-01 02:25:42 +0300

    Added 2nd data type test sample

--- bloom_filter.py
+++ bloom_filter.py
@@ -10,6 +10,9 @@ class BloomFilter:
     def check(self, item):
         index = hash(item) % self.size
         return self.bit_array[index] == 1
+    # Add check for false positive rate against size
+    # Add check false positive rate if size was exceeded
+    # Add check for compression rate (false positive rate vs expected false positive rate)
 
 # Example 
 bloom = BloomFilter(10)  # just for demostration
@@ -21,3 +24,14 @@ bloom.add("banana")
 print("apple", bloom.check("apple"))  # Should be True
 print("banana", bloom.check("banana"))  # Should be True
 print("cherry", bloom.check("cherry"))  # Should be False
+
+# Example 2 - Testing Different Data Type
+bloom = BloomFilter(10)
+
+bloom.add("TTAATTA")
+bloom.add("ATTACTC")
+
+# Check for existing and non-existing items
+print("TTAATTA", bloom.check("TTAATTA"))  # Should be True
+print("ATTACTC", bloom.check("ATTACTC"))  # Should be True
+print("ACTCAC", bloom.check("ACTCAC"))  # Should be False
