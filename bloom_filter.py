7f434740755f5a78103d39217d1032e31264d0fc
andrea-maza-leon <andrea.mazaleon@student.uhasselt.be>
2024-05-09 15:41:53 -0700

    Deleting tests in the main bloom filter

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,4 +1,4 @@
-## bloom_filter.py      # The main module with the Bloom filter implementation
+# The main module with the Bloom filter implementation
 import hashlib
 
 def hash_functions(item, num_hashes, size):
@@ -23,33 +23,3 @@ class BloomFilter:
     def check(self, item):
         indices = hash_functions(item, self.num_hashes, self.size)
         return all(self.bit_array[index] for index in indices)
-    # Add check for false positive rate against size
-    # Add check false positive rate if size was exceeded
-    # Add check for compression rate (false positive rate vs expected false positive rate)
-
-def test_false_positives(bloom_filter, items, test_items):
-    for item in items:
-        bloom_filter.add(item)
-           
-    false_positives = sum(1 for item in test_items if bloom_filter.check(item))
-    print(f"False positive rate: {false_positives / len(test_items):.2%}")
-
-# Example 
-bloom = BloomFilter(1000, 4)  # just for demostration
-
-existing_items = ["apple", "banana", "orange"]
-test_items = ["grape", "mango", "lemon"] + existing_items
-
-test_false_positives(bloom, existing_items, test_items)
-
-
-# Example 2 - Testing Different Data Type
-bloom = BloomFilter(1000, 4)
-
-bloom.add("TTAATTA")
-bloom.add("ATTACTC")
-
-# Check for existing and non-existing items
-print("TTAATTA", bloom.check("TTAATTA"))  # Should be True
-print("ATTACTC", bloom.check("ATTACTC"))  # Should be True
-print("ACTCAC", bloom.check("ACTCAC"))  # Should be False
